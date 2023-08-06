61a4095cea6a846634a0cf257e82e6284870d8e7
86446 <864460295@qq.com>
2023-08-06 11:18:16 +0800

    Force merge

---
+++ src/main/java/me/noryea/mixin/AoFaceDataMixin.java
@@ -0,0 +1,15 @@
+package me.noryea.mixin;
+
+import me.jellysquid.mods.sodium.client.model.light.smooth.SmoothLightPipeline;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AoFaceData.class)
+public class AoFaceDataMixin {
+	@Inject(at = @At("HEAD"), method = "loadWorld")
+	private void init(CallbackInfo info) {
+		// This code is injected into the start of MinecraftServer.loadWorld()V
+	}
+}
